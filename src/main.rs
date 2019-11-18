76008c37c1d6ae3315394f91a377fe1d0590489c
wenewzhang <wenewboy@gmail.com>
2019-11-18 23:42:30 +0800

    hashmap

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,27 @@
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
 
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
-}
-
-impl Rectangle {
-    fn square(size: u32) -> Rectangle {
-        Rectangle { width: size, height: size }
-    }
-}
 
 fn main() {
-    let rect1 = Rectangle { width: 30, height: 50 };
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        rect1.area()
-    );
-    let rect2 = Rectangle::square(3);
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        rect2.area()
-    );
+        use std::collections::HashMap;
+
+        let text = "hello world wonderful world";
+
+        let mut map = HashMap::new();
+
+        for word in text.split_whitespace() {
+            let count = map.entry(word).or_insert(0);
+            *count += 1;
+        }
+
+    /*
+    This code will print {"world": 2, "hello": 1, "wonderful": 1}.
+    The or_insert method actually returns a mutable reference (&mut V)
+    to the value for this key. Here we store that mutable reference in
+    the count variable, so in order to assign to that value, we must
+    first dereference count using the asterisk (*). The mutable reference
+    goes out of scope at the end of the for loop, so all of these
+    changes are safe and allowed by the borrowing rules.
+    */
+        println!("{:?}", map);
+
 }
